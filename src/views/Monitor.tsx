7b9688128d7f70ca9082291a173854ad58717a29
Alexis Ken Alvarez <alexisken1432@gmail.com>
2024-04-25 22:24:24 +0800

    added clickable to youtube link

--- src/views/Monitor.tsx
+++ src/views/Monitor.tsx
@@ -6,11 +6,18 @@ const Monitor = () => {
   return (
     <Container>
       <div className="mx-auto flex max-w-screen-xl flex-col items-center justify-center py-20">
-        <img src="/computer.webp" alt="Computer" className="" />
-          
-        <div className="mt-10 flex items-center justify-center flex-wrap gap-10">
+        <a href="https://youtu.be/s39uqm3B-OA?si=bw_7QVxj0BILi1v9" rel="noopener noreferrer" target="_blank">
+          <img src="/computer.webp" alt="Computer" className="" />
+        </a>
+
+        <div className="mt-10 flex flex-wrap items-center justify-center gap-10">
           {tech.map((items) => (
-            <img src={`/tech/${items}.png`} key={items} alt={items} className="md:w-auto w-32" />
+            <img
+              src={`/tech/${items}.png`}
+              key={items}
+              alt={items}
+              className="w-32 md:w-auto"
+            />
           ))}
         </div>
       </div>
